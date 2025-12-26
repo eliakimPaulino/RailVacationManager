4429dbe8140ed1ff242ae8080c6eccaf41dbde4c
eliakim <eliakim.paulino@gmail.com>
2025-12-26 19:39:47 -0300

    refactor: update imports and restructure project files

--- bin/rail_vacation_manager.dart
+++ bin/rail_vacation_manager.dart
@@ -1,4 +1,4 @@
-import 'package:rail_vacation_manager/rail_vacation_manager.dart' as rail_vacation_manager;
+import 'package:rail_vacation_manager/main.dart' as rail_vacation_manager;
 
 void main(List<String> arguments) {
   print('Hello world: ${rail_vacation_manager.calculate()}!');
--- test/rail_vacation_manager_test.dart
+++ test/rail_vacation_manager_test.dart
@@ -1,4 +1,4 @@
-import 'package:rail_vacation_manager/rail_vacation_manager.dart';
+import 'package:rail_vacation_manager/main.dart';
 import 'package:test/test.dart';
 
 void main() {
