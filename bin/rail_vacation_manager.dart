014ee97ec53c968fd17138723b249fee380a691f
eliakim <eliakim.paulino@gmail.com>
2025-12-26 17:01:12 -0300

    initial setup

---
+++ bin/rail_vacation_manager.dart
@@ -0,0 +1,5 @@
+import 'package:rail_vacation_manager/rail_vacation_manager.dart' as rail_vacation_manager;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rail_vacation_manager.calculate()}!');
+}
---
+++ test/rail_vacation_manager_test.dart
@@ -0,0 +1,8 @@
+import 'package:rail_vacation_manager/rail_vacation_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
