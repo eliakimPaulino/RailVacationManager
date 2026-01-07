68ef27847c5186257025fa6dc6e6e93c55876332
eliakim <eliakim.paulino@gmail.com>
2026-01-06 21:55:39 -0300

    test: add unit test for ApproveVacationUseCase to validate vacation approval

---
+++ test/application/use_cases/approve_vacation_use_case.dart
@@ -0,0 +1,40 @@
+import 'package:rail_vacation_manager/application/use_cases/approve_vacation_use_case.dart';
+import 'package:rail_vacation_manager/core/vacation_status.dart';
+import 'package:rail_vacation_manager/infrastructure/repositories/in_memory_employee_repository.dart';
+import 'package:rail_vacation_manager/infrastructure/repositories/in_memory_vacation_repository.dart';
+import 'package:test/test.dart';
+
+import '../../helpers/test_fixtures.dart';
+
+void main() {
+  late InMemoryVacationRepository vacationRepo;
+  late InMemoryEmployeeRepository employeeRepo;
+  late ApproveVacationUseCase useCase;
+
+  setUp(() {
+    vacationRepo = InMemoryVacationRepository();
+    employeeRepo = InMemoryEmployeeRepository();
+    useCase = ApproveVacationUseCase(vacationRepo, employeeRepo);
+  });
+
+  test('should approve a request vacation', () async {
+    final vr = createTestApprovedVacationRequest(
+      requestId: 'req-1',
+      employeeId: 'emp-1',
+      start: DateTime(2026, 01, 06),
+      end: DateTime(2026, 01, 25),
+    );
+
+    vr.reject(vr.employeeId);
+
+    await vacationRepo.save(vr);
+
+    final result = await useCase.execute(
+      requestId: 'req-1',
+      approverIdRaw: 'emp-1',
+    );
+
+    expect(result.isSuccess, true);
+    expect(result.value.status, VacationStatus.Approved);
+  });
+}
