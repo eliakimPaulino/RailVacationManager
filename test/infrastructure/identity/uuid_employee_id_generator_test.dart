fde0a05b9af5fb2f7974004ba0a20dd548203a74
eliakim <eliakim.paulino@gmail.com>
2026-01-11 21:58:18 -0300

    test: add unit test for UuidEmployeeIdGenerator to validate ID generation

---
+++ test/infrastructure/identity/uuid_employee_id_generator_test.dart
@@ -0,0 +1,43 @@
+import 'package:rail_vacation_manager/core/clock.dart';
+import 'package:rail_vacation_manager/infrastructure/identity/uuid_employee_id_generator.dart';
+import 'package:test/test.dart';
+import 'package:uuid/data.dart';
+import 'package:uuid/uuid.dart';
+
+class FakeUuid extends Uuid {
+  final String value;
+
+  FakeUuid(this.value);
+
+  @override
+  String v4({V4Options? config, Map<String, dynamic>? options}) => value;
+}
+
+class FakeClock extends Clock {
+  final DateTime fixed;
+
+  FakeClock(this.fixed);
+
+  @override
+  DateTime now() => fixed;
+}
+
+void main () {
+  test('happy path: should generate id mixing uuid and timestamp', () {
+    // arrange
+    final fakeUuid = FakeUuid('123e4567-e89b-12d3-a456-426614174000');
+    final fakeClock = FakeClock(DateTime(2025, 1, 1, 12, 0, 0));
+
+    final generator = UuidEmployeeIdGenerator(fakeUuid, fakeClock);
+    
+    // act
+    final result = generator.generate();
+
+    //assert
+    expect(result.isSuccess, true);
+
+    final empId = result.value;
+
+    expect(empId.value, '123e4567-e89b-12d3-a456-426614174000-2025-01-01T12:00:00.000');
+  });
+}
